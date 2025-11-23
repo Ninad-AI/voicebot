e0c20649bfd9f485d39e7162c4d3dbd2b1589d2f
de-arnavgupta <arnavgpt28@outlook.com>
2025-11-23 06:52:38 +0530

    integrated with the backend

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,8 +23,17 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      react,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-uses-vars': 'error',
     },
   },
 ])
